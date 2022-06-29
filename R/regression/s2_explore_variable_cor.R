3451c4e200b23a09c1126907cdb07bc0384338c7
ryurko <ryurko21@gmail.com>
2022-06-29 15:06:12 -0400

    Add script for 5-fold cv testing different subsets of variables

--- R/regression/s2_explore_variable_cor.R
+++ R/regression/s2_explore_variable_cor.R
@@ -47,7 +47,77 @@ ggpairs(nfl_model_data,
         aes(alpha = .5))
 
 
+# Perform 5-fold CV -------------------------------------------------------
+
+set.seed(2020)
+nfl_model_data <- nfl_model_data %>%
+  mutate(test_fold = sample(rep(1:5, length.out = n())))
 
 
+# Get cross-validation predictions
+# INPUTS: model_formula, dataset (where dataset assumed to have test_fold)
+# OUTPUT: Dataset containing test fold predictions
 
+get_cv_preds <- function(model_formula, dataset = nfl_model_data) {
+  
+  map_dfr(unique(dataset$test_fold),
+          function(holdout_i) {
+            
+            # Separate and get the training and test data
+            test_data <- dataset %>%
+              filter(test_fold == holdout_i)
+            
+            train_data <- dataset %>%
+              filter(test_fold != holdout_i)
+            
+            # Fit the model on training data:
+            train_model <-
+              lm(as.formula(model_formula), data = train_data)
+            
+            # Return a table of holdout predictions:
+            tibble(test_preds = predict(train_model, newdata = test_data),
+                   test_actual = test_data$score_diff,
+                   test_fold = holdout_i)
+            
+          })
+  
+}
 
+# Get CV results with different sets of variables:
+all_cv_preds <- get_cv_preds("score_diff ~  offense_ave_epa_pass + offense_ave_epa_run + defense_ave_epa_pass + defense_ave_epa_run")
+all_int_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_pass*offense_ave_epa_run + defense_ave_epa_pass*defense_ave_epa_run")
+run_only_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_run + defense_ave_epa_run")
+pass_only_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_pass + defense_ave_epa_pass")
+off_only_cv_preds <- get_cv_preds("score_diff ~ offense_ave_epa_pass + offense_ave_epa_run")
+def_only_cv_preds <- get_cv_preds("score_diff ~ defense_ave_epa_pass + defense_ave_epa_run")
+int_only_cv_preds <- get_cv_preds("score_diff ~ 1")
+
+bind_rows(mutate(all_cv_preds, type = "All"),
+          mutate(all_int_cv_preds, type = "All w/ interactions"),
+          mutate(pass_only_cv_preds, type = "Passing only"),
+          mutate(run_only_cv_preds, type = "Running only"),
+          mutate(off_only_cv_preds, type = "Offense only"),
+          mutate(def_only_cv_preds, type = "Defense only"),
+          mutate(int_only_cv_preds, type = "Intercept-only")) %>%
+  group_by(type) %>%
+  summarize(rmse = sqrt(mean((test_actual - test_preds)^2))) %>%
+  mutate(type = fct_reorder(type, rmse)) %>%
+  ggplot(aes(x = type, y = rmse)) +
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  coord_flip()
+
+bind_rows(mutate(all_cv_preds, type = "All"),
+          mutate(all_int_cv_preds, type = "All w/ interactions"),
+          mutate(pass_only_cv_preds, type = "Passing only"),
+          mutate(run_only_cv_preds, type = "Running only"),
+          mutate(off_only_cv_preds, type = "Offense only"),
+          mutate(def_only_cv_preds, type = "Defense only"),
+          mutate(int_only_cv_preds, type = "Intercept-only")) %>%
+  group_by(type, test_fold) %>%
+  summarize(rmse = sqrt(mean((test_actual - test_preds)^2))) %>%
+  mutate(type = fct_reorder(type, rmse)) %>%
+  ggplot(aes(x = type, y = rmse)) +
+  geom_point(alpha = .8) +
+  theme_bw() +
+  coord_flip()
